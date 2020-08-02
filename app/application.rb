1e8a187b33ce663b6cf333e0066704063178d0fe
hellonathanchung <chung.nathanh@gmail.com>
2020-08-02 16:33:40 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -10,3 +10,12 @@ class Application
     resp.write "#{num_1}\n"
     resp.write "#{num_1}\n"
    
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win"
+    else 
+      resp.write "You Lose"
+    end
+
+    resp.finish
+  end
+end
